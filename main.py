166fa7a157841c978f67dc65aec86c97eb73ef7b
yahyathegreat <yahyamardan1234@gmail.com>
2025-02-05 18:39:24 +0500

    space invader

--- main.py
+++ main.py
@@ -25,3 +25,41 @@ enemyY = []
 enemyX_change = []
 enemyY_change = []
 num_of_enemies = 6
+for _i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randit(0, SCREEN_WIDTH - 64))
+    enemyY.append(random.randit(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
+bulletImg = pygame.image.load('bulllet.png')
+bulletX = 0
+bulletY = PLAYER_START_Y
+bulletX_change = 0
+bullet_change = BULLET_SPEED_Y
+bullet_state = "ready"
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+def show_score(x,y):
+    score = font.render("score : " + str(score_value), True, (255, 255, 255))
+    screen.bit(score, (x,y))
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+def player(x, y):
+    screen.blit(playerImg, (x,y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x,y))
+def fir_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, ( x+ 16, y + 10))
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(enemyX - bulletX) ** 2 + (enemyY - bulletY)( ** 2)
+    return distance < COLLISION_DISTANCE
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
